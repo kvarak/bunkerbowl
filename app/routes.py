7b8d81af23e2feab3a430e73080244f7cf2cde5d
Albert Rigo <albert.rigo@gmail.com>
2020-12-25 19:04:11 +0100

    checkpoint

--- app/routes.py
+++ app/routes.py
@@ -20,9 +20,17 @@ credential = ServiceAccountCredentials.from_json_keyfile_name(
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"])
 client = gspread.authorize(credential)
-bunkerbowl7sheet = "1v7QeeAKkV0y7GNDfpZntD4242YPo9jTeB-xLYjuLrFI"
-#gsheet = client.open(bunkerbowl7sheet).worksheet("games")
-gsheet = client.open_by_key(bunkerbowl7sheet).worksheet("games")
+
+bunkerbowlsheet = {}
+bunkerbowlsheet[7] = "1v7QeeAKkV0y7GNDfpZntD4242YPo9jTeB-xLYjuLrFI"
+bunkerbowlsheet[6] = "1GWV0D0LdhuokvxsgO38MYLQmbhndLbpOAaC79HLCQYU"
+bunkerbowlsheet[5] = "1kJS7jsdvfCkvO07HCmQybbqpn9wmX58wOqhshoLUZ6U"
+bunkerbowlsheet[4] = "1qwYVySOod7C1JfXmNWytCkkI9IQQS2M6u25epmOsPzs"
+bunkerbowlsheet[3] = "http://holdtheground.blogspot.se/2017/11/bunker-bowl-season-3.html"
+bunkerbowlsheet[2] = "1BY2FAPiFnEqDB84bnnga4nSfoeN7R7AHWQjFi2xGEYI"
+bunkerbowlsheet[1] = "1c_ksQ5azUB0YsW2u_uait0Aty4crIR6qy1dSMVXoxSU"
+
+bunkerbowlhistory = "1WKhsUERByCLcnOqSHWgJimNav7wY-QNvtCATINZzYIg"
 
 
 # --------------------------------------------------------------------------------
@@ -31,23 +39,74 @@ gsheet = client.open_by_key(bunkerbowl7sheet).worksheet("games")
 
 @app.route('/')
 @app.route('/index')
-@app.route('/season')
-@app.route('/regler')
-@app.route('/statistik')
 def index():
 
-    # a_yaml_file = open('data/skills.yml')
-    # parsed_yaml_file = yaml.load(a_yaml_file, Loader=yaml.FullLoader)
-
-    # states = parsed_yaml_file['config']['states']
-    # skills = parsed_yaml_file['skills']
+    try:
+        gsheet = client.open_by_key(bunkerbowlhistory).worksheet("seasons")
+    except:
+        return render_template(
+            'main.html')
 
     data = gsheet.get_all_records()
-#    print(data)
 
     return render_template(
         'main.html',
-        data=data)
+        data=data,
+        season=season)
 
 # --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
 
+@app.route('/season', methods=['GET', 'POST'])
+def season():
+
+    season = int(request.values.get('s'))
+
+    try:
+        gsheet = client.open_by_key(bunkerbowlsheet[season]).worksheet("games")
+    except:
+        if season == 3:
+            text = bunkerbowlsheet[season]
+        else:
+            text = ""
+        return render_template(
+            'season.html',
+            text=text,
+            season=season)
+
+    data = gsheet.get_all_records()
+    # print(data)
+
+    return render_template(
+        'season.html',
+        data=data,
+        season=season)
+
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+
+@app.route('/regler')
+def regler():
+
+    return render_template(
+        'regler.html')
+
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+
+@app.route('/statistik')
+def statistik():
+
+    return render_template(
+        'statistik.html')
+
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+
+@app.route('/team')
+def team():
+
+    return render_template(
+        'team.html')
+
+# --------------------------------------------------------------------------------
